355260594ce3e24572cacea65ce05e04a5c07b2c
ksv97 <kosov.r.m@gmail.com>
2024-07-29 12:00:20 +0300

    Add SQL task

---
+++ GetProductsWithCategories.sql
@@ -0,0 +1,41 @@
+﻿/*
+В базе данных MS SQL Server есть продукты и категории. Одному продукту может соответствовать много категорий, 
+в одной категории может быть много продуктов. Напишите SQL запрос для выбора всех пар «Имя продукта – Имя категории». 
+Если у продукта нет категорий, то его имя все равно должно выводиться.
+По возможности — положите ответ рядом с кодом из первого вопроса.
+
+*/
+
+-- Предполагаемая архитектура БД
+
+/*
+
+table Products(
+
+Id: int (PK)
+Name: varchar(150) 
+CategoryId: int (FK) NULL
+)
+
+table Categories(
+
+Id: int (PK)
+Name: varchar(50)
+
+)
+
+Запрос:
+
+SELECT p.Name AS "Имя продукта", 
+     (CASE 
+        WHEN p.CategoryId IS NULL 
+            THEN "Без категории" 
+        ELSE c.Name 
+      END) AS "Имя категории"
+FROM
+Products p
+LEFT JOIN
+Categories c
+ON p.CategoryId = c.Id
+
+*/
